9d3e9bf4fd2b593465c7f7c1d2ff95f40cb138c0
aarshika <61321778+aarshika@users.noreply.github.com>
2020-02-21 15:54:04 +0100

    Assignment 1

---
+++ Assignment 1p2.c
@@ -0,0 +1,17 @@
+/*
+   CH-230-B
+   a1_p2.c
+   Aarshika Singh
+   aa.singh@jacobs-university.de
+*/
+#include<stdio.h>
+int main()
+{
+    int result;
+    /*The result of our calculation*/
+    result=(((2+7)*9)/3);
+    printf("The result is %d\n",result);
+    return 0;
+    /*The answer was coming to be 4200624 because the variable result was not being inserted in the printf
+    function.*/
+}
